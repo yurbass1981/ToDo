ed348e33de99d0d93baa4f129eb3a889cf9c8833
yurbass <yurbass1@rambler.ru>
2022-12-11 20:03:39 +0300

    Added inmemory and infile profiles

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-using Microsoft.DotNet.Scaffolding.Shared.ProjectModel;
 using ToDo.Repositories;
 using ToDo.Repositories.Implemention;
 using ToDo.Services;
@@ -6,19 +5,21 @@ using ToDo.Services.Implemention;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-//if (app.Environment.IsDevelopment())
-//{
-//    builder.Services.AddScoped<IToDoRepository, InMemoryToDoRepository>();
-//}
-//else 
-//{
-//    builder.Services.AddScoped<IToDoRepository, InFileToDoRepository>();
-//}
-
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddSingleton<IToDoRepository, InMemoryToDoRepository>();
+
+var storageType = builder.Configuration.GetSection("StorageType").Value;
+if (storageType == "InMemory")
+{
+    builder.Services.AddSingleton<IToDoRepository, InMemoryToDoRepository>();
+}
+
+if (storageType == "InFile")
+{
+    builder.Services.AddScoped<IToDoRepository, InFileToDoRepository>();
+}
+
+
 builder.Services.AddScoped<IToDoService, ToDoService>();
 
 var app = builder.Build();
