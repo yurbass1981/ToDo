22d795cc384f910b83954c5a73ed886f93d10444
yurbass <yurbass1@rambler.ru>
2022-12-24 12:47:27 +0300

    Added mapping

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -7,87 +7,83 @@ namespace ToDo.Controllers;
 
 public class TodoController : Controller
 {
-    private readonly ITodoService _todoService;
+   private readonly ITodoService _todoService;
 
-    public TodoController(ITodoService todoService)
-    {
-        _todoService = todoService;
-    }
+   public TodoController(ITodoService todoService)
+   {
+      _todoService = todoService;
+   }
 
-    public IActionResult List()
-    {
-        var todoItemDtoList = _todoService.GetList();
-        var viewModelList = new List<TodoViewModel>();
+   public IActionResult List()
+   {
+      var todoItemDtoList = _todoService.GetList();
+      var viewModelList = new List<TodoViewModel>();
 
 
-        //TODO: we need to learn LINQ and rewrite this part of code to LINQ expression
-        foreach (var todoDto in todoItemDtoList)
-        {
-            var todoViewModel = new TodoViewModel
-            {
-                Id = todoDto.Id,
-                Text = todoDto.Text,
-                Created = todoDto.Created,
-                IsCompleted = todoDto.IsCompleted
-            };
+      //TODO: we need to learn LINQ and rewrite this part of code to LINQ expression
+      foreach (var todoDto in todoItemDtoList)
+      {
+         var todoViewModel = MapDtoToModel(todoDto);
+         viewModelList.Add(todoViewModel);
+      }
+      return View(viewModelList);
+   }
 
-            viewModelList.Add(todoViewModel);
-        }
-        return View(viewModelList);
-    }
+   public IActionResult Delete(Guid id)
+   {
+      _todoService.Delete(id);
+      return RedirectToAction("List");
+   }
 
-    public IActionResult Delete(Guid id)
-    {
-        _todoService.Delete(id);
-        return RedirectToAction("List");
-    }
+   public IActionResult Create(TodoViewModel todoViewModel)
+   {
+      //TODO: we're already setting the Id and Created fields in the _todoService.Create method
+      //so... no need to do it here, all that we need to map is text
+      //no...
+      //I came up with a brillian idea it's better to create map functions that will 
+      // be converting from dto to viewModel and the other way around
+      var todoItemDto = new TodoItemDto()
+      {
+         Id = Guid.NewGuid(),
+         Text = todoViewModel.Text,
+         Created = DateTime.Now
+      };
 
-    public IActionResult Create(TodoViewModel todoViewModel)
-    {
-        //TODO: we're already setting the Id and Created fields in the _todoService.Create method
-        //so... no need to do it here, all that we need to map is text
-        //no...
-        //I came up with a brillian idea it's better to create map functions that will 
-        // be converting from dto to viewModel and the other way around
-        var todoItemDto = new TodoItemDto()
-        {
-            Id = Guid.NewGuid(),
-            Text = todoViewModel.Text,
-            Created = DateTime.Now
-        };
+      _todoService.Create(todoItemDto);
+      return RedirectToAction("List");
+   }
 
-        _todoService.Create(todoItemDto);
-        return RedirectToAction("List");
-    }
+   public IActionResult UpdateView(Guid id)
+   {
+      var todoDto = _todoService.GetById(id);
 
-    public IActionResult UpdateView(Guid id)
-    {
-        var todoDto = _todoService.GetById(id);
+      var todoViewModel = MapDtoToModel(todoDto);
+      return View("Update", todoViewModel);
+   }
 
-        //TODO: the same that I said above (in the Create action) we need to create map functions and reuse mapping functionality
-        var todoViewModel = new TodoViewModel
-        {
-            Id = todoDto.Id,
-            Text = todoDto.Text,
-            Created = todoDto.Created,
-            IsCompleted = todoDto.IsCompleted
-        };
+   public IActionResult Update(Guid id, TodoViewModel todoViewModel)
+   {
+      //TODO: the same that I said above (in the Create action) we need to create map functions and reuse mapping functionality
+      var todoItemDto = new TodoItemDto
+      {
+         Id = todoViewModel.Id,
+         Text = todoViewModel.Text,
+         Created = todoViewModel.Created,
+         IsCompleted = todoViewModel.IsCompleted
+      };
 
-        return View("Update", todoViewModel);
-    }
+      _todoService.Update(id, todoItemDto);
+      return RedirectToAction("List");
+   }
 
-    public IActionResult Update(Guid id, TodoViewModel todoViewModel)
-    {
-        //TODO: the same that I said above (in the Create action) we need to create map functions and reuse mapping functionality
-        var todoItemDto = new TodoItemDto
-        {
-            Id = todoViewModel.Id,
-            Text = todoViewModel.Text,
-            Created = todoViewModel.Created,
-            IsCompleted = todoViewModel.IsCompleted
-        };
-
-        _todoService.Update(id, todoItemDto);
-        return RedirectToAction("List");
-    }
+   private TodoViewModel MapDtoToModel(TodoItemDto todoItemDto)
+   {
+      return new TodoViewModel
+      {
+         Id = todoItemDto.Id,
+         Text = todoItemDto.Text,
+         Created = todoItemDto.Created,
+         IsCompleted = todoItemDto.IsCompleted
+      };
+   }
 }
--- Services/Implementation/TodoService.cs
+++ Services/Implementation/TodoService.cs
@@ -5,50 +5,43 @@ namespace ToDo.Services.Implementation;
 
 public class TodoService : ITodoService
 {
-    private readonly IToDoRepository _toDoRepository;
+   private readonly IToDoRepository _toDoRepository;
 
-    public TodoService(IToDoRepository toDoRepository)
-    {
-        _toDoRepository = toDoRepository;
-    }
+   public TodoService(IToDoRepository toDoRepository)
+   {
+      _toDoRepository = toDoRepository;
+   }
 
 
-    public List<TodoItemDto> GetList()
-    {
-        return _toDoRepository.GetList();
-    }
+   public List<TodoItemDto> GetList()
+   {
+      return _toDoRepository.GetList();
+   }
 
-    public void Create(TodoItemDto todoItem)
-    {
-        var newToDoItem = new TodoItemDto
-        {
-            Id = Guid.NewGuid(),
-            Text = todoItem.Text,
-            Created = DateTime.Now
-        };
+   public void Create(TodoItemDto todoItem)
+   {
+      _toDoRepository.Create(todoItem);
+   }
 
-        _toDoRepository.Create(newToDoItem);
-    }
+   public void Update(Guid id, TodoItemDto todoItem)
+   {
+      _toDoRepository.Update(id, todoItem);
+   }
 
-    public void Update(Guid id, TodoItemDto todoItem)
-    {
-        _toDoRepository.Update(id, todoItem);
-    }
+   public void Delete(Guid id)
+   {
+      _toDoRepository.Delete(id);
+   }
 
-    public void Delete(Guid id)
-    {
-        _toDoRepository.Delete(id);
-    }
+   public TodoItemDto GetById(Guid id)
+   {
+      var itemById = _toDoRepository.GetById(id);
 
-    public TodoItemDto GetById(Guid id)
-    {
-        var itemById = _toDoRepository.GetById(id);
+      if (itemById == null)
+      {
+         throw new Exception($"Todo item with id {id} hasn't been found");
+      }
 
-        if (itemById == null)
-        {
-            throw new Exception($"Todo item with id {id} hasn't been found");
-        }
-
-        return itemById;
-    }
+      return itemById;
+   }
 }
