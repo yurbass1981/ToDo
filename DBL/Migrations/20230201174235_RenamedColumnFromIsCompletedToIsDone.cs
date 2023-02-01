894fc525f3d450af62a7ce37a87860c580abba41
yurbass <yurbass1@rambler.ru>
2023-02-01 20:53:14 +0300

    added new migration

---
+++ DBL/Migrations/20230201174235_RenamedColumnFromIsCompletedToIsDone.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDo.DBL.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenamedColumnFromIsCompletedToIsDone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsCompleted",
+                table: "TodoItems",
+                newName: "IsDone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsDone",
+                table: "TodoItems",
+                newName: "IsCompleted");
+        }
+    }
+}
